cffce22fcea3ceef0c7fd438e3de61aafb70d735
add-IV <47333498+add-IV@users.noreply.github.com>
2024-10-27 02:42:14 +0100

    add size compute

--- src/ps_1/sparse_data_util.py
+++ src/ps_1/sparse_data_util.py
@@ -1,8 +1,9 @@
-from data_util import get_um_by_name as get_um_by_name_data_util
-from sparse_algorithms import center_matrix_and_store_non_nan
+from .data_util import get_um_by_name as get_um_by_name_data_util
+from .sparse_algorithms import center_matrix_and_store_non_nan
 import pandas as pd
 import numpy as np
 import polars as pl
+import scipy.sparse as sp
 
 
 
@@ -13,9 +14,16 @@ def get_size_in_mb(obj):
         return obj.nbytes / (1024 * 1024)
     elif isinstance(obj, pd.DataFrame):
         return obj.memory_usage(deep=True).sum() / (1024 * 1024)
+    elif isinstance(obj, sp.csr_matrix):
+        return obj.data.nbytes / (1024 * 1024) + obj.indices.nbytes / (1024 * 1024) + obj.indptr.nbytes / (1024 * 1024)
     else:
         return None
     
 def get_um_by_name(config, dataset_name):
     um = get_um_by_name_data_util(config, dataset_name)
     return center_matrix_and_store_non_nan(um)
+
+
+if __name__ == "__main__":
+    um_lecture, means, matrix_non_nan = get_um_by_name(None, "lecture_1")
+# end main
